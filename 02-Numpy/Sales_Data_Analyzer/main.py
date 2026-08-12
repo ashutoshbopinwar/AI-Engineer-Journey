300db86b79f214c1c4ce54c9d319de7eb87a83a0
ashutoshbopinwar <ashutoshbopinwar@gmail.com>
2026-08-13 00:30:10 +0530

    created sample sales data set

---
+++ 02-Numpy/Sales_Data_Analyzer/main.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+# create a sample sales data array with days and prices
+
+# Days 
+days = np.array(["Monday","Tuesday","Wednesday","Thursday","Friday","Saturday","Sunday"])
+
+# Products
+products = np.array(["Laptop","Phone","Headphones","Keyboard","Mouse"])
+
+#  Sales Quantity
+sales = np.array([
+       [5, 10, 15, 8, 20],
+    [7, 12, 18, 10, 25],
+    [6, 15, 12, 9, 22],
+    [8, 14, 20, 11, 28],
+    [10, 18, 25, 15, 35],
+    [12, 22, 30, 18, 40],
+    [9, 16, 22, 13, 32]
+])
